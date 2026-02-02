babf96ac23455c85d134fc47a64d1a5780f3278b
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-02-02 09:32:25 +0530

    Time: 0 ms (100.00%) | Memory: 31 MB (10.94%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i-1] > nums[i]){
+                cnt++;
+                if(cnt > 1) return false;
+
+                if(i >= 2 && nums[i] < nums[i-2]) nums[i] = nums[i-1];
+            }
+        }
+        return true;
+    }
+};
