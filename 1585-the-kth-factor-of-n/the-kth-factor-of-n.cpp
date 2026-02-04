3911e15c092fa452d41c3305ab6a3b2d69ecf919
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-02-04 11:22:31 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (52.17%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int kthFactor(int n, int k) {
-        int cnt = 0;
-        for(int i=1; i<=n; i++){
+        // int cnt = 0;
+        // for(int i=1; i<=n; i++){
+        //     if(n % i == 0){
+        //         cnt += 1;
+        //         if(cnt == k) return i;
+        //     }
+        // }
+        // return -1;
+
+        int root = sqrt(n);
+        for(int i = 1; i <= root; i++){
             if(n % i == 0){
-                cnt += 1;
-                if(cnt == k) return i;
+                k--;
+                if(k == 0) return i;
+            }
+        }
+        for(int i = root; i >= 1; i--){
+            if(n % i == 0 && i != n/i){
+                k--;
+                if(k == 0) return n/i;
             }
         }
         return -1;
