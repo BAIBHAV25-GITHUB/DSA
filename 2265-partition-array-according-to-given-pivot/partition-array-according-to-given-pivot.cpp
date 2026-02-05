07bb7d407eae1854a6d4eacaf9807295284cfc82
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-02-05 09:25:27 +0530

    Time: 1 ms (91.29%) | Memory: 127.7 MB (77.71%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> ans;
-        for(int x : nums)
-            if(x < pivot) ans.push_back(x);
-        for(int x : nums)
-            if(x == pivot) ans.push_back(pivot);
-        for(int x : nums)
-            if(x > pivot) ans.push_back(x);
+        // vector<int> ans;
+        // for(int x : nums)
+        //     if(x < pivot) ans.push_back(x);
+        // for(int x : nums)
+        //     if(x == pivot) ans.push_back(pivot);
+        // for(int x : nums)
+        //     if(x > pivot) ans.push_back(x);
     
+        // return ans;
+
+        int n = nums.size();
+        vector<int> ans(n);
+        int l=0, r=n-1;
+        for(int i=0; i<n; i++){
+            if(nums[i] < pivot) ans[l++] = nums[i];
+        }
+
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i] > pivot) ans[r--] = nums[i];
+        }
+
+        while(l <= r) ans[l++] = pivot;
+
         return ans;
     }
 };
