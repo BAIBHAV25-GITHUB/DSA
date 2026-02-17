f94c342cb488ada229f28e75d609c2752ba36c13
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-02-17 12:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (65.38%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> ans;
+        for(int HH=0; HH <= 11; HH++){
+            for(int MH=0; MH <= 59; MH++){
+                if(__builtin_popcount(HH) + __builtin_popcount(MH) == turnedOn){
+                    string hour = to_string(HH);
+                    string minute = (MH < 10 ? "0" : "") + to_string(MH);
+                    ans.push_back(hour + ":" + minute);
+                }
+            }
+        }
+        return ans;
+    }
+};
