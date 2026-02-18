9048d57cd0a1164ffff731f1518ab7b4a8ae678d
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-02-18 11:43:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (91.43%) - LeetSync

--- 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/binary-number-with-alternating-bits.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        int curr = n % 2;
-        n /= 2;
-        while(n > 0){
-            if(curr == n%2) return false;
+        // T(n) = O(logn);
+        // int curr = n % 2;
+        // n /= 2;
+        // while(n > 0){
+        //     if(curr == n%2) return false;
 
-            curr = n % 2;
-            n /= 2;
-        }
-        return true;
+        //     curr = n % 2;
+        //     n /= 2;
+        // }
+        // return true;
+
+        unsigned int res = n ^ (n >> 1);
+        return ((res & (res+1)) == 0);
     }
 };
