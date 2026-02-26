bf7c0dcf4a4d86bb731ea02acf434ceec2698ade
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-02-26 13:54:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (58.55%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    void plusOne(string& s){
-        int i = s.size()-1;
-        while(i >= 0 && s[i] != '0'){
-            s[i] = '0';
-            i--;
-        }
-        if(i < 0){
-            s = '1' + s;
-        }
-        else s[i] = '1';
-    }
-    int numSteps(string s) {
-        int ops = 0;
-        while(s.size() > 1){
-            int n = s.size();
-            if(s[n-1] == '0') s.pop_back();
-            else{
-                plusOne(s);
+    // void plusOne(string& s){
+    //     int i = s.size()-1;
+    //     while(i >= 0 && s[i] != '0'){
+    //         s[i] = '0';
+    //         i--;
+    //     }
+    //     if(i < 0){
+    //         s = '1' + s;
+    //     }
+    //     else s[i] = '1';
+    // }
+    // int numSteps(string s) {
+    //     int ops = 0;
+    //     while(s.size() > 1){
+    //         int n = s.size();
+    //         if(s[n-1] == '0') s.pop_back();
+    //         else{
+    //             plusOne(s);
+    //         }
+    //         ops++;
+    //     }
+    //     return ops;
+    // }
+
+    int numSteps(string s){
+        int n = s.size(), ops = 0, carry = 0;
+        for(int i = n-1; i >= 1; i--){
+            if(((s[i] - '0') + carry) % 2 == 1){
+                ops += 2;
+                carry = 1;
             }
-            ops++;
+            else ops += 1;
         }
-        return ops;
+        return ops + carry;
     }
 };
