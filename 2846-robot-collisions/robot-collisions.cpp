b0f71da16daf0f4b0b4517f5633f7c160af403cb
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-04-01 11:51:55 +0530

    Time: 47 ms (90.82%) | Memory: 194.8 MB (66.47%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<int> index(n);
+        for(int i = 0; i < n; i++){
+            index[i] = i;
+        }
+
+        sort(index.begin(), index.end(), [&](int a, int b){
+            return positions[a] < positions[b];
+        });
+
+        stack<int> st;
+        for(int i : index){
+            if(directions[i] == 'R'){
+                st.push(i);
+            }
+            else{
+                while(!st.empty() && healths[i] > 0){
+                    int j = st.top();
+                    st.pop();
+
+                    if(healths[j] > healths[i]){
+                        healths[j]--;
+                        healths[i] = 0;
+                        st.push(j);
+                    }
+                    else if(healths[i] > healths[j]){
+                        healths[i]--;
+                        healths[j] = 0;
+                    }
+                    else{
+                        healths[i] = 0;
+                        healths[j] = 0;
+                    }
+                }
+            }
+        }
+
+        vector<int> ans;
+        for (int i = 0; i < n; i++){
+            if(healths[i] > 0) ans.push_back(healths[i]);
+        }
+
+        return ans;
+    }
+};
