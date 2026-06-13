5cd02b45b2e9981a8a091290a7364c77cb2e03e1
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-06-13 15:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (99.78%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,29 +11,47 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if(root == nullptr) return ans;
+    void rightView(TreeNode* root, vector<int>& res, int level){
+        if(root == nullptr) return;
 
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            vector<int> level;
-            int size = q.size();
-            for(int i=0; i<size; i++){
-                TreeNode* node=q.front();
-                q.pop();
-                level.push_back(node->val);
-                
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-            }
-            ans.push_back(level.back());
+        if(level == res.size()) res.push_back(root->val);
+        if(root->right){
+            rightView(root->right, res, level+1);
         }
+        if(root->left){
+            rightView(root->left, res, level+1);
+        }
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        // Picking the last node of each level.. T(n)-O(n), S(n)-O(n);
+        // vector<int> ans;
+        // if(root == nullptr) return ans;
+
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     vector<int> level;
+        //     int size = q.size();
+        //     for(int i=0; i<size; i++){
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         level.push_back(node->val);
+                
+        //         if(node->left){
+        //             q.push(node->left);
+        //         }
+        //         if(node->right){
+        //             q.push(node->right);
+        //         }
+        //     }
+        //     ans.push_back(level.back());
+        // }
+        // return ans;
+
+        //Using DFS T(n)-O(n), S(n)-O(h)
+        vector<int> ans;
+        rightView(root, ans, 0);
         return ans;
     }
 };
