af175dffed527762c5604b14aa10449a25770f85
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-08-11 00:32:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (11.29%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
-        if(n==0 || n==1) return n;
+    // int solveUsingMemo(int n, vector<int>& dp){
+    //     if(n==0 || n==1) return n;
 
-        if(dp[n] != -1){
-            return dp[n];
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+
+    //     int ans = fib(n-1) + fib (n-2);
+    //     dp[n] = ans;
+    //     return dp[n];
+    // } 
+
+    int solveUsingTab(int n){
+        vector<int> dp(n+1, -1);
+        dp[0] = 0, dp[1] = 1;
+
+        for(int i=2; i<=n ; i++){
+            dp[i] = dp[i-1]+dp[i-2];
         }
 
-        int ans = fib(n-1) + fib (n-2);
-        dp[n] = ans;
         return dp[n];
-    } 
+    }
+
     int fib(int n) {
-        vector<int> dp(n+1, -1);
-        int ans = solve(n, dp);
+        // vector<int> dp(n+1, -1);
+        // int ans = solveusingMemo(n, dp);
+        // return ans;
+        if(n == 0 || n == 1) return n;
+        int ans = solveUsingTab(n);
         return ans;
     }
 };
