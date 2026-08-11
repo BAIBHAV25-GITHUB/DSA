1737dc1116441dcdd8d0736218f1e435554a2ed7
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-08-11 11:38:04 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (97.83%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
+        // Tabulation Method
+        // int n = nums.size();
 
-        if(n == 1) return nums[0];
-        
-        vector<int> dp(n, -1);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for(int i=2; i<n; i++){
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        // if(n == 1) return nums[0];
+
+        // vector<int> dp(n, -1);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+        // for(int i=2; i<n; i++){
+        //     dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        // }
+        // return dp[n-1];
+
+        // Space Optimized
+        int prev1 = 0, prev2 = 0, curr;
+        for(int i=0; i<nums.size(); i++){
+            curr = max(prev1, prev2+nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n-1];
+        return curr;
     }
 };
