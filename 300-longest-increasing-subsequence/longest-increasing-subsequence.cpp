dd49966bc9471fca638faf8c39241ad326321def
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-08-25 21:30:55 +0530

    Time: 299 ms (29.49%) | Memory: 296.4 MB (22.20%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,22 +1,41 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int curr, int prev, vector<vector<int>>& dp){
-        if(curr >= nums.size()) return 0;
-        if(dp[curr][prev+1] != -1) return dp[curr][prev+1]; 
+    // int solveUsingMem(vector<int>& nums, int curr, int prev, vector<vector<int>>& dp){
+    //     if(curr >= nums.size()) return 0;
+    //     if(dp[curr][prev+1] != -1) return dp[curr][prev+1]; 
 
-        int include = 0;
-        if(prev == -1 || nums[curr] > nums[prev]){
-            include = 1+solve(nums, curr+1, curr, dp);
+    //     int include = 0;
+    //     if(prev == -1 || nums[curr] > nums[prev]){
+    //         include = 1+solveUsingMem(nums, curr+1, curr, dp);
+    //     }
+    //     int exclude = 0+solveUsingMem(nums, curr+1, prev, dp);
+    //     dp[curr][prev+1] = max(include, exclude);
+    //     return dp[curr][prev+1];
+    // }
+
+    int solveUsingTab(vector<int>& nums){
+        int n=nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int curr=n-1; curr>=0; curr--){
+            for(int prev=curr-1; prev>= -1; prev--){
+                int include = 0;
+                if(prev == -1 || nums[curr] > nums[prev]){
+                    include = 1+dp[curr+1][curr+1];
+                }
+                int exclude = dp[curr+1][prev+1];
+                dp[curr][prev + 1] = max(include, exclude);
+            }
         }
-        int exclude = 0+solve(nums, curr+1, prev, dp);
-        dp[curr][prev+1] = max(include, exclude);
-        return dp[curr][prev+1];
+        return dp[0][0];
     }
+
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
         // int prev=-1, curr=0;
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
         
-        return solve(nums,0, -1, dp);
+        // return solveUsingMem(nums,0, -1, dp);
+
+        return solveUsingTab(nums);
     }
 };
