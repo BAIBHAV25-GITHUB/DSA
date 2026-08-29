394e70f23afa87d29670114826c2c78b9f284982
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-08-29 12:49:27 +0530

    Time: 155 ms (33.80%) | Memory: 111.2 MB (21.02%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -12,19 +12,41 @@ public:
     //     return(include || exclude);
     // }
 
-    bool solveUsingMem(vector<int>& nums, int target, int index, int n, vector<vector<int>>& dp){
-        if(index >= n) return false;
-        if(target < 0) return false;
+    // bool solveUsingMem(vector<int>& nums, int target, int index, int n, vector<vector<int>>& dp){
+    //     if(index >= n) return false;
+    //     if(target < 0) return false;
 
-        if(target == 0) return true;
+    //     if(target == 0) return true;
 
-        if(dp[index][target] != -1) return dp[index][target];
+    //     if(dp[index][target] != -1) return dp[index][target];
 
-        bool include = solveUsingMem(nums, target-nums[index], index+1, n, dp);
-        bool exclude = solveUsingMem(nums, target, index+1, n, dp);
+        // bool include = solveUsingMem(nums, target-nums[index], index+1, n, dp);
+        // bool exclude = solveUsingMem(nums, target, index+1, n, dp);
 
-        dp[index][target] = (include || exclude);
-        return dp[index][target];
+        // dp[index][target] = (include || exclude);
+        // return dp[index][target];
+    // }
+
+    bool solveUsingTab(vector<int>& nums, int target){
+        int n=nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+
+
+        for(int i=0; i<n; i++){
+            dp[i][0]= 1;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            for(int t=1; t<=target; t++){
+                bool include = 0;
+                if(t-nums[i] >= 0) include = dp[i+1][t-nums[i]];
+                bool exclude = dp[i+1][t];
+
+                dp[i][t] = (include || exclude);
+            }
+        }
+
+        return dp[0][target];
     }
 
     bool canPartition(vector<int>& nums) {
@@ -36,7 +58,9 @@ public:
 
         int target = sum/2;
 
-        vector<vector<int>> dp(n, vector<int>(target+1, -1));
-        return solveUsingMem(nums, target, 0, n, dp);
+        // vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        // return solveUsingMem(nums, target, 0, n, dp);
+
+        return solveUsingTab(nums, target);
     }
 };
