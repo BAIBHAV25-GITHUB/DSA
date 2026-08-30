a3f03fac63efe958717d909abdfe31fdcda9c6a5
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-08-30 12:24:34 +0530

    Time: 32 ms (88.10%) | Memory: 11.2 MB (7.26%) - LeetSync

--- 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
+++ 375-guess-number-higher-or-lower-ii/guess-number-higher-or-lower-ii.cpp
@@ -10,21 +10,42 @@ public:
     //     return ans;
     // }
 
-    int solveUsingMem(int start, int end,vector<vector<int>>& dp){
-        if(start >= end) return 0;
-        if(dp[start][end] != -1) return dp[start][end];
+    // int solveUsingMem(int start, int end,vector<vector<int>>& dp){
+    //     if(start >= end) return 0;
+    //     if(dp[start][end] != -1) return dp[start][end];
         
-        int ans = INT_MAX;
-        for(int i=start; i<= end; i++){
-            ans = min(ans, i+max(solveUsingMem(start, i-1, dp), solveUsingMem(i+1, end, dp)));
+    //     int ans = INT_MAX;
+    //     for(int i=start; i<= end; i++){
+    //         ans = min(ans, i+max(solveUsingMem(start, i-1, dp), solveUsingMem(i+1, end, dp)));
+    //     }
+    //     return dp[start][end] = ans;
+    // }
+
+    int solveUsingTab(int n){
+
+        vector<vector<int>> dp(n+2, vector<int>(n+2, 0));
+        
+        for(int start=n; start >= 1; start--){
+            for(int end=1; end <= n; end++){
+                if(start >= end) continue;
+                else{
+                    int ans = INT_MAX;
+                    for(int i=start; i<= end; i++){
+                        ans = min(ans, i+max(dp[start][i-1], dp[i+1][end]));
+                    }
+                    dp[start][end] = ans;
+                }
+            }
         }
-        return dp[start][end] = ans;
+        return dp[1][n];
     }
 
     int getMoneyAmount(int n) {
         // return solveUsingRec(1, n);
 
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return solveUsingMem(1, n, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        // return solveUsingMem(1, n, dp);
+
+        return solveUsingTab(n);
     }
 };
