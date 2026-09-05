7fec48b7e6bb9f3af33c43aa832c07384f1f0ddb
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-09-06 00:02:16 +0530

    Time: 7 ms (61.29%) | Memory: 13.2 MB (61.45%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -16,23 +16,45 @@ public:
     //     return ans;
     // }
 
-    int solveUsingMem(string& a, string& b, int i, int j, vector<vector<int>>& dp){
-        if(i == a.length()) return b.length()-j;
-        if(j == b.length()) return a.length()-i;
+    // int solveUsingMem(string& a, string& b, int i, int j, vector<vector<int>>& dp){
+    //     if(i == a.length()) return b.length()-j;
+    //     if(j == b.length()) return a.length()-i;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int ans = 0;
-        if(a[i] == b[j]) ans = solveUsingMem(a, b, i+1, j+1, dp);
-        else{
-            int insert = 1+solveUsingMem(a, b, i, j+1, dp);
-            int deleted = 1+solveUsingMem(a, b, i+1, j, dp);
-            int replace = 1+solveUsingMem(a, b, i+1, j+1, dp);
+    //     int ans = 0;
+    //     if(a[i] == b[j]) ans = solveUsingMem(a, b, i+1, j+1, dp);
+    //     else{
+    //         int insert = 1+solveUsingMem(a, b, i, j+1, dp);
+    //         int deleted = 1+solveUsingMem(a, b, i+1, j, dp);
+    //         int replace = 1+solveUsingMem(a, b, i+1, j+1, dp);
 
-            ans = min(insert, min(deleted, replace));
+    //         ans = min(insert, min(deleted, replace));
+    //     }
+    //     dp[i][j] = ans;
+    //     return ans;
+    // }
+
+    int solveUsingTab(string& a, string& b){
+        int n=a.length(), m=b.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for(int i=0; i<=n; i++) dp[i][m] = n-i;
+        for(int j=0; j<=m; j++) dp[n][j] = m-j;
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0 ; j--){
+                if(a[i] == b[j]) dp[i][j] = dp[i+1][j+1];
+                else{
+                    int insert = 1+dp[i][j+1];
+                    int deleted = 1+dp[i+1][j];
+                    int replace = 1+dp[i+1][j+1];
+
+                    dp[i][j] = min(insert, min(deleted, replace));
+                }
+            }
         }
-        dp[i][j] = ans;
-        return ans;
+        return dp[0][0];
     }
 
     int minDistance(string word1, string word2) {
@@ -42,7 +64,9 @@ public:
 
         // return solveUsingRec(word1, word2, 0, 0);
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return solveUsingMem(word1, word2, 0, 0, dp);
+        // vector<vector<int>> dp(n, vector<int>(m, -1));
+        // return solveUsingMem(word1, word2, 0, 0, dp);
+
+        return solveUsingTab(word1, word2);
     }
 };
