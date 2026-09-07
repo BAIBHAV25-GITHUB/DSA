3cd3de19b4424ce3c001b704d49e769b931bdf01
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-09-07 23:27:33 +0530

    Time: 3 ms (9.51%) | Memory: 9.4 MB (30.53%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // int solveUsingRec(int m, int n, int i, int j){
+    //     if(i >= m || j >= n) return 0;
+    //     if(i == m-1 && j == n-1) return 1;
+
+    //     return solveUsingRec(m, n, i+1, j) + solveUsingRec(m, n, i, j+1);
+    // }
+
+    int solveUsingMem(int m, int n, int i, int j, vector<vector<int>>& dp){
+        if(i >= m || j >= n) return 0;
+        if(i == m-1 && j == n-1) return 1;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        dp[i][j] = solveUsingMem(m, n, i+1, j, dp) + solveUsingMem(m, n, i, j+1, dp);
+        return dp[i][j];
+    }
+
+    int uniquePaths(int m, int n) {
+        // return solveUsingRec(m, n, 0, 0);
+
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solveUsingMem(m, n, 0, 0, dp);
+    }
+};
