7033cc7b1853a7640d0901a53c67e4f440987dbd
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-09-09 10:35:43 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (73.35%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -11,43 +11,71 @@ public:
     //     return min(down, right);
     // }
 
-    int solveUsingMem(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
+    // int solveUsingMem(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
+    //     int m = grid.size(), n = grid[0].size();
+    //     if(i >= m || j >= n) return 1e9;
+    //     if(i == m-1 && j == n-1) return grid[m-1][n-1];
+
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     int down = grid[i][j] + solveUsingMem(grid, i+1, j, dp);
+    //     int right = grid[i][j] + solveUsingMem(grid, i, j+1, dp);
+
+    //     return dp[i][j] = min(down, right);
+    // }
+
+    // int solveUsingTab(vector<vector<int>>& grid){
+    //     int m = grid.size(), n = grid[0].size();
+    //     vector<vector<int>> dp(m, vector<int>(n, 0));
+
+    //     for(int i=m-1; i>=0; i--){
+    //         for(int j=n-1; j>=0; j--){
+    //             if(i == m-1 && j == n-1){
+    //                 dp[i][j] = grid[i][j];
+    //             }
+    //             else{
+    //                 int down = grid[i][j];
+    //                 if(i < m-1) down += dp[i+1][j];
+    //                 else down += 1e9;
+
+    //                 int right = grid[i][j];
+    //                 if(j < n-1) right += dp[i][j+1];
+    //                 else right += 1e9;
+
+    //                 dp[i][j] = min(down, right);
+    //             }
+    //         }
+    //     }
+
+    //     return dp[0][0];
+    // }
+
+    int solve(vector<vector<int>>& grid){
         int m = grid.size(), n = grid[0].size();
-        if(i >= m || j >= n) return 1e9;
-        if(i == m-1 && j == n-1) return grid[m-1][n-1];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int down = grid[i][j] + solveUsingMem(grid, i+1, j, dp);
-        int right = grid[i][j] + solveUsingMem(grid, i, j+1, dp);
-
-        return dp[i][j] = min(down, right);
-    }
-
-    int solveUsingTab(vector<vector<int>>& grid){
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> next(n+1, 0);
 
         for(int i=m-1; i>=0; i--){
+            vector<int> curr(n+1, 0);
             for(int j=n-1; j>=0; j--){
                 if(i == m-1 && j == n-1){
-                    dp[i][j] = grid[i][j];
+                    curr[j] = grid[i][j];
                 }
                 else{
                     int down = grid[i][j];
-                    if(i < m-1) down += dp[i+1][j];
+                    if(i < m-1) down += next[j];
                     else down += 1e9;
 
                     int right = grid[i][j];
-                    if(j < n-1) right += dp[i][j+1];
+                    if(j < n-1) right += curr[j+1];
                     else right += 1e9;
 
-                    dp[i][j] = min(down, right);
+                    curr[j] = min(down, right);
                 }
             }
+            next = curr;
         }
 
-        return dp[0][0];
+        return next[0];
     }
 
     int minPathSum(vector<vector<int>>& grid) {
@@ -57,6 +85,8 @@ public:
         // vector<vector<int>> dp(m, vector<int>(n, -1));
         // return solveUsingMem(grid, 0, 0, dp);
 
-        return solveUsingTab(grid);
+        // return solveUsingTab(grid);
+
+        return solve(grid);
     }
 };
