24375208061df8fd17795bd87816b44476ddc9ba
BAIBHAW_25@GIT <148851068+BAIBHAV25-GITHUB@users.noreply.github.com>
2026-09-10 14:14:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (44.91%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    // int solveUsingRec(vector<vector<int>>& triangle, int i, int j){
+    //     int n=triangle.size();
+    //     if(i == n) return 0;
+
+    //     int down = triangle[i][j] + solveUsingRec(triangle, i+1, j);
+    //     int right = triangle[i][j] + solveUsingRec(triangle, i+1, j+1);
+
+    //     return min(down, right);
+    // } 
+
+    // int solveUsingMem(vector<vector<int>>& triangle, int i, int j, vector<vector<int>>& dp){
+    //     int n=triangle.size();
+    //     if(i == n) return 0;
+
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     int down = triangle[i][j] + solveUsingMem(triangle, i+1, j, dp);
+    //     int right = triangle[i][j] + solveUsingMem(triangle, i+1, j+1, dp);
+
+    //     dp[i][j] = min(down, right);
+    //     return dp[i][j];
+    // }
+
+    int solveUsingTab(vector<vector<int>>& triangle){
+        int n=triangle.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<i+1; j++){
+                int down = triangle[i][j] + dp[i+1][j];
+                int right = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(down, right);
+            }
+        }
+        return dp[0][0];
+    } 
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        // return solveUsingRec(triangle, 0, 0);
+
+        // int n=triangle.size();
+        // vector<vector<int>> dp(n, vector<int>(n, -1));
+        // return solveUsingMem(triangle, 0, 0, dp);
+
+        return solveUsingTab(triangle);
+    }
+};
